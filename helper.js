0a6244f39c3a656f10a47501974c0ac212cf04c2
Joseph Maynard <joseph.maynard@mobileembrace.com>
2016-04-11 10:53:58 +1000

    Add Param function, refactor preload to use load image, rename loadPixel to loadImg

--- helper.js
+++ helper.js
@@ -1,5 +1,9 @@
 h = {
+
 	el: {},
+
+	h = this,
+
 	create: function(name, type, parent){
 		if(!this.el.hasOwnProperty(name)){
 			this.el[name] = document.createElement(type);
@@ -13,13 +17,22 @@ h = {
 			h.style(this, attributes);
 		}
 	},
+
 	style: function(element, attributes){
 		var prefix = ['transform', 'transition', 'perspective', 'transformStyle', 'transitionTimingFunction', 'backfaceVisibility'];
 		for(style in attributes){
 			if(prefix.indexOf(style) > -1){
+				element.style['ms' + style.charAt(0).toUpperCase() + style.slice(1)] = attributes[style];
+				element.style['moz' + style.charAt(0).toUpperCase() + style.slice(1)] = attributes[style];
 				element.style['webkit' + style.charAt(0).toUpperCase() + style.slice(1)] = attributes[style];
 			}
 			element.style[style] = attributes[style];
+		}
+	},
+
+	param: function(element, attributes){
+		for(param in attributes){
+			element[param] = attributes[param];
 		}
 	},
 
@@ -30,16 +43,14 @@ h = {
 	preloadImages: function(imagePath, imageArray, callBack){
 		var preloadedImages = [];
 		for(var i = 0; i < imageArray.length; i++){
-			preloadedImages[i] = new Image();
-			preloadedImages[i].src = imagePath + imageArray[i];
-			preloadedImages[i].onload = function(){
+			this.loadImg(imagePath + imageArray[i], function(){
 				preloadCounter++;
 				if(preloadCounter == imageArray.length && typeof callBack === "function") callBack();
-			}
+			});
 		}
 	},
 
-	firePixel: function(url, callBack){
+	loadImg: function(url, callBack){
 		var pixel = new Image();
 		pixel.src = url;
 		if(typeof callBack === "function"){
